ff9e21110749ed9196b9d6059fbd9062597c036c
vermanil <anilrajverma1996@gmail.com>
2017-02-14 15:23:46 +0530

    create inverted index

---
+++ invertedIndex.py
@@ -0,0 +1,190 @@
+# import ElementTree to parse the hindi document and read according to tag
+import xml.etree.ElementTree as ET
+# import string to find the list of string punctuation 
+import string 
+# import glob to read all document of particuler folder
+import glob
+# import pickle to store dictionary in unreadable format of .p extension
+import pickle
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Make a class of inverted index in which all the process is present to find inverted index
+class invertedIndex():
+	# This is an constructor which will start fetch the document word and finally will make an inverted index
+	def __init__(self):
+		# self.exclude is an array which have punctuation of string
+		self.exclude = list(string.punctuation)
+		self.exclude.append('।') #append one hindi punctuation 
+		#print(exclude)
+		#open file to write
+		self.f4 = open("wordIndex.txt", 'w')
+		#read all file from the hindi folder and store in list(fileName)
+		fileName = glob.glob("hindi/*.txt")
+		fileName.sort() # sort the list to find sequence of file
+		self.doc_id = 1 
+		#self.doc_id (To store the document name with some id)
+		#self.indexDict is an dictionary to store the posting list
+		self.indexDict = {}
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+		#we apply all the operation in one-one file which comes sequentially using loop and store it as posting list.
+		for i in range(0,len(fileName)):
+			#Apply try catch because some document have invalid XML character(&""''<>) which can not parse
+			try:
+				print(fileName[i])
+				#parse the document using ElementTree
+				tree = ET.parse(fileName[i])
+				root = tree.getroot()
+				text = ''
+				#read the content of title tag and store in self.text1 variable
+				for i in root.findall('title'):
+					self.text1 = i.text
+				#read the content of content tag and store in self.text2 variable
+				for i in root.findall('content'):
+					self.text2 = i.text
+				#now read content store in one variable
+				text = self.text1 + self.text2
+				#call the first operation(Remove punctuation from the document) and update the value of text variable
+				text = self.remove_punctuation(text)
+				#call the second operation(Remove stop word from the document) and store it in text variable in form of array
+				text = self.remove_stop_word(text)
+				#call the operation to apply stemming on word and store it with their document name in posting list
+				self.make_PostingList(text)
+				#self.doc_id = self.doc_id + 1
+			except:
+				#catch the error if document can not be parse
+				print(fileName[i])
+				f= open(fileName[i],'r').read()
+				#read the content of file like substring between tag title
+				start = f.find('<title>')+len('<title>')
+				end = f.find('</title')
+				self.text1 = f[start:end]
+				#read the content of file like substring between tag content
+				start = f.find('<content>')+len('<content>')
+				end = f.find('</content')
+				self.text2 = f[start:end]
+				text = self.text1 + self.text2
+				print(text)
+				text = self.remove_punctuation(text)
+				#call the second operation(Remove stop word from the document) and store it in text variable in form of array
+				tex = self.remove_stop_word(text)
+				#call the operation to apply stemming on word and store it with their document name in posting list
+				self.make_PostingList(tex)
+				#print(self.doc_id)
+			finally:
+				#increment the document id because for different document it will have different id
+				self.doc_id = self.doc_id + 1
+				#print(self.doc_id)
+				#source.close()
+		#After all this we the posting list in the file
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		for i in self.indexDict.keys():
+			self.f4.write(i + "\t\t\t")
+			self.f4.write(str(self.indexDict.get(i)) + "\n")
+		self.f4.close()
+
+	# def write_in_one_file(self, text):
+	# 	self.f = open("allInOne.txt", 'a+')
+	# 	self.f.write(text)
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#An operation to apply stemming and store word with their document id and frequency
+	def make_PostingList(self, word):
+		#appy stemming on each word and store in lsit(word)
+		word = [self.stem_word(w) for w in word]
+		#convert the list into set data type and again convert to into list to find the unique word of that file
+		l = set(word)
+		wordUnique = list(l)
+		for i in range(len(wordUnique)):
+			c=word.count(str(wordUnique[i]))
+			#make condition that if word present in another document then append the current document at that place otherwise make another row and store it there
+			if self.doc_id > 1:
+				if str(wordUnique[i]) in self.indexDict.keys():
+					self.indexDict[str(wordUnique[i])][self.doc_id] = c
+					# self.f4.write(str(wordUnique[i]) + '\t\t\t')
+					# self.f4.write(str(c) + '\t\t\t')
+					# self.f4.write("doc" + str(self.doc_id) + '\n')
+				else:
+					self.indexDict[str(wordUnique[i])] = {}
+					self.indexDict[str(wordUnique[i])][self.doc_id] = c
+					#self.indexDict[str(wordUnique[i])] = [0]
+					# self.f4.write(str(wordUnique[i]) + '\t\t\t')
+					# self.f4.write(str(c) + '\t\t\t')
+					# self.f4.write("doc" + str(self.doc_id) + '\n')
+
+			else:
+				#print(self.doc_id)
+				self.indexDict[str(wordUnique[i])] = {}
+				self.indexDict[str(wordUnique[i])][self.doc_id] = 1
+				# self.f4.write(str(wordUnique[i]) + '\t\t\t')
+				# self.f4.write(str(c) + '\t\t\t')
+				# self.f4.write("doc" + str(self.doc_id) + '\n')
+			#print(documentLocations)
+
+		#print(self.indexDict)
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#An operation to remove stop word
+	def remove_stop_word(self, text):
+		#print("removing stop word")
+		#listOut all the stop word in stopWord list 
+		stopWord=open("stopWord.txt", "r").read().split("\n")
+		text3 = []
+		#print(stopWord)
+		text = text.split()
+		#make condition if text are stop word then it will remove
+		for c in text:
+			if c not in stopWord:
+				text3.append(c)
+				#print(c)
+		# tex = "".join(c for c in text if c not in stopWord)
+		return text3
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+#An operation to remove punctuation
+	def remove_punctuation(self, text):
+		text = "".join(c for c in text if c not in self.exclude)
+		return text
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#An operation to find the root word by doing stemming
+#(Taken from Github)
+	def stem_word(self,word):
+		#suffixes have all the suffixes of hindi word which mostly comes at the end of word
+		suffixes = {
+		    1: ["ो","े","ू","ु","ी","ि","ा"],
+		    2: ["कर","ाओ","िए","ाई","ाए","ने","नी","ना","ते","ीं","ती","ता","ाँ","ां","ों","ें"],
+		    3: ["ाकर","ाइए","ाईं","ाया","ेगी","ेगा","ोगी","ोगे","ाने","ाना","ाते","ाती","ाता","तीं","ाओं","ाएं","ुओं","ुएं","ुआं"],
+		    4: ["ाएगी","ाएगा","ाओगी","ाओगे","एंगी","ेंगी","एंगे","ेंगे","ूंगी","ूंगा","ातीं","नाओं","नाएं","ताओं","ताएं","ियाँ","ियों","ियां"],
+		    5: ["ाएंगी","ाएंगे","ाऊंगी","ाऊंगा","ाइयाँ","ाइयों","ाइयां"],
+		}
+
+		#make condition if word will end with these suffixes then that part will remove 
+		for k in 5, 4, 3, 2, 1:
+			if len(word) > k:
+					#print('h')
+				for s in suffixes[k]:
+					if word.endswith(s):
+						#print(sf)
+						return (word[:-k])
+			
+		return word
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+if __name__ == "__main__":
+	#finally call all the operation by making the object of the class
+	import os
+	if os.path.isfile("wordIndex.p") != True:
+		obj = invertedIndex()
+		#finally dump the posting list in wordIndex.p in binary format
+		pickle.dump(obj.indexDict,open( "wordIndex.p", "wb" ))
+	indexDict = {}
+	indexDict = pickle.load(open("wordIndex.p", "rb" ))
+	print(len(indexDict))
+	while 1:
+		word = input("enter word u want posting list\n")
+		print(indexDict[word])
+		if 44 in indexDict[word].keys():
+			print('yes')
+		else:
+			print('no')
