c6d29caf6b7e48acda6a9b26fc33ee9e05bdf4c6
vermanil <anilrajverma1996@gmail.com>
2017-03-28 20:04:34 +0530

    make documet vec and query vector

--- english_Index.py
+++ english_Index.py
@@ -10,29 +10,35 @@ import sys
 import os
 import glob
 from nltk import PorterStemmer
+import collections
 import string
 import pickle
+import math
 
 class english_Index():
 	def __init__(self):
 		f = open("path.txt","r").read()
 		path = (f.split())
+		# print(path)
 		self.flag = 0
 		self.fileName = []
 		for i in range(0,len(path)):
 			p = path[i] + "/"
 			for file in os.listdir(p):
 				self.fileName.append(p + file)
-		self.english_dict = {}
 
 		print(len(self.fileName))
+		# print(self.fileName)
 
 
 	def start(self):
 		# print("start")
+		self.english_dict = {}
 		N = len(self.fileName)
-		for i in range(0,2):
+		for i in range(0,100):
 			# print(self.fileName[i])
+			if(i%100 == 0):
+				print(i)
 			f= open(self.fileName[i],'r').read()
 			#read the content of file like substring between tag title
 			self.text1 = ""
@@ -55,8 +61,8 @@ class english_Index():
 			text = self.remove_punctuation(text)
 			text2 = self.remove_stopWord(text)
 			text2 = self.stemming(text2)
-			self.makePostingList(text2)	
-		# print(self.english_dict)
+			self.makePostingList(text2,i)	
+		print(self.english_dict['door'])
 
 	def remove_punctuation(self, text):
 		# print("remove")
@@ -78,10 +84,12 @@ class english_Index():
 	def stemming(self,text):
 		# print("stemming")
 		s = PorterStemmer()
-		root = [s.stem(w) for w in text]
+		root = []
+		for w in text:
+			root.append(s.stem(w))
 		return root
 
-	def makePostingList(self,text):
+	def makePostingList(self,text,ind):
 		# print("posting")
 		l = set(text)
 		wordUnique = list(l)
@@ -90,16 +98,33 @@ class english_Index():
 			#make condition that if word present in another document then append the current document at that place otherwise make another row and store it there
 			if self.flag != 0:
 				if wordUnique[i] in self.english_dict.keys():
-					self.english_dict[str(wordUnique[i])][self.doc_id] = c
+					self.english_dict[str(wordUnique[i])][self.fileName[i]] = c
 
 				else:
 					self.english_dict[str(wordUnique[i])] = {}
-					self.english_dict[str(wordUnique[i])][self.doc_id] = c
+					self.english_dict[str(wordUnique[i])][self.fileName[i]] = c
 
 			else:
 				self.english_dict[str(wordUnique[i])] = {}
-				self.english_dict[str(wordUnique[i])][self.doc_id] = 1
+				self.english_dict[str(wordUnique[i])][self.fileName[i]] = 1
 				self.flag = 1
+
+	def getLength(self,doc):
+		f = open(doc,"r").read()
+		if(f.find('<TITLE>') != -1):
+			start = f.find('<TITLE>')+len('<TITLE>')
+			end = f.find('</TITLE')
+			self.text1 = f[start:end]
+			#read the content of file like substring between tag content
+		if(f.find('<TEXT>') != -1):
+			start = f.find('<TEXT>')+len('<TEXT>')
+			end = f.find('</TEXT')
+			self.text2 = f[start:end]
+		text = self.text1 + self.text2
+		return len(text)
+
+	def dotProduct(self, Qvector, Dvector):
+		return sum([x*y for x,y in zip(Qvector,Dvector)])
 
 if __name__ == "__main__":
 	if os.path.isfile("path.txt") != True:
@@ -113,6 +138,60 @@ if __name__ == "__main__":
 		f.close()
 	else:
 		# path = []
+		if os.path.isfile("englishIndex.p") != True:
+			obj = english_Index()
+			obj.start()
+			pickle.dump(obj.english_dict,open( "englishIndex.p", "wb" ))
+
 		obj = english_Index()
-		obj.start()
-		pickle.dump(obj.english_dict,open( "englishWordIndex.p", "wb" ))
+		english_dict = {}
+		english_dict = pickle.load(open("englishIndex.p", "rb" ))
+		# print(len(english_dict))
+
+		query = input("enter your query\n")
+		query = obj.remove_punctuation(query)
+		query = obj.remove_stopWord(query)
+		query1 = set(query)
+		query1 = list(query)
+		length = len(query1)
+		# print(obj.stemming(query1))
+		docVector = collections.defaultdict(lambda: [0] * length)
+		queryVector = [0]*length
+		N = len(obj.fileName)
+		for q in range(0,len(query1)):
+			Qtf = query.count(query1[q])
+			idf = math.log(N/(len(query1)))
+			queryVector[q] = Qtf * idf
+		# queryVector.insert(q,Qtf)
+		sumQvector = sum([x for x in queryVector])
+		# print(queryVector)
+		queryVector = [(x)/sumQvector for x in queryVector]
+		# print(queryVector)
+		query = obj.stemming(query)
+		j = 0
+		for i in query:
+			OccuresDocument = english_dict[i]
+			df = len(OccuresDocument)
+			idf = math.log(N/df)
+			# flag =0
+			for o in OccuresDocument.keys():
+				tf = english_dict[i][o]
+				w = (tf)*(idf)
+				# queryLenght.insert(i,w)
+				docVector[o][j] = w
+			j = j+1
+		# docVec = sorted(docVector.items())
+		# print(docVector)
+		for doc, weight in docVector.items():
+			#print(obj.fileName[doc-1])
+			docLength = obj.getLength(doc)
+			print(docLength)
+			for i in range(0,len(weight)):
+			# 	#print("hello")
+			# 	# print((docVector[doc][i]))
+			 	docVector[doc][i] = (docVector[doc][i])/(docLength)
+		print(docVector)
+
+		Scores=[ [obj.dotProduct(DocVec, queryVector), doc] for doc, DocVec in docVector.items() ]
+		Scores.sort(reverse = True)
+		print([x[1] for x in Scores[:20]])
--- readAllFilePath.sh
+++ readAllFilePath.sh
@@ -6,12 +6,14 @@ cd en.docs.2011/
 flag=0
 fl=0
 i=0
+loop=-1
 export foo="hi"
 path=()
 for d in `ls`;
 do 
 	#var s = "../en.docs.2011/" + "$d"
 	# echo $d
+	loop=$((loop+1))
 	if [ $flag -eq 1 ]; 
 		then
 		if [ $fl -eq 1 ];
@@ -26,25 +28,65 @@ do
 	cd $d
 	for subdir in `ls`;
 	do 
-		# echo $subdir
 		if [ $fl -eq 1 ]; 
 		then
 			cd ..
 		fi
+		# echo $subdir
+		# if [ -d "$subdir" ]; then
+  		# Control will enter here if $DIRECTORY exists.
 		fl=1
+		# echo $subdir
+		if [ $tele -eq 1 ]; then 
+			cd ..
+		fi
+		tele=0
 		cd $subdir
-		s=`pwd`
-		#echo $s
-		path[$i]=$s
-		# echo $s
-		i=$((i+1))
+		if [ $loop -eq 1 ]; then
+			for a in `ls`;
+			do
+				if [[ "$a" == *utf8 ]]; then
+    				# echo "yes"
+    				cd $a
+    				for q in `ls`; 
+    				do
+    					cd $q
+    					s=`pwd`
+						path[$i]=$s
+						i=$((i+1))
+						cd ..
+    				done
+					cd ..
+				else
+					pwd
+					# echo $a
+					if [ $tele -eq 1 ]; then 
+						cd ..
+					fi
+					tele=1
+					cd $a
+					s=`pwd`
+					path[$i]=$s
+					i=$((i+1))
+				fi
+			done
+		else
+			s=`pwd`
+			path[$i]=$s
+			# echo $s
+			i=$((i+1))
+		fi
 	done
 # echo "${path[@]}"
-done	
+done
+# echo "${path[@]}"	
 # declare -a joined;
 # joined=( $(./english_Index.py "${path[@]}" ) );
 cd ..
 cd ..
 cd ..
+if [ $tele -eq 1 ]; then 
+	cd ..
+fi
 cd FinalIR
 python3 english_Index.py "${path[@]}"
